9de8d5fd6191893df108c019513fedc0141e7221
alegrm <alegrm@gmail.com>
2014-06-27 11:34:23 +0200

    initial commit

---
+++ download/repo.php
@@ -0,0 +1,92 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="UTF-8" />
+<meta name="author" content="Vadim Zaslawski" />
+<?php
+$suite = $_GET["suite"];
+switch ($suite)
+{
+case "nightly":
+	$suiteSuffix = "-nightly";
+	break;
+case "testing":
+	$suiteSuffix = "-testing";
+	break;
+default:
+	$suite		 = "stable";
+	$suiteSuffix = "";
+}
+?>
+<title><?= ucfirst($suite) ?> Repository – Linked Data Stack</title>
+<link rel="stylesheet" href="repo.css" />
+
+</head>
+<body>
+
+<a href="rss.php?suite=<?= $suite ?>" style="float: right;">RSS</a>
+
+<a href="index.html">Repositories</a>:
+<a href="repo.php">Stable</a>,
+<a href="repo.php?suite=testing">Testing</a>,
+<a href="repo.php?suite=nightly">Nightly</a>
+
+<table>
+	<caption>Linked Data Stack: <strong><?= ucfirst($suite) ?></strong> Repository</caption>
+	<tr>
+		<th><a href="?suite=<?= $suite ?>">Package</a></th>
+		<th>Version</th>
+		<th><a href="?suite=<?= $suite ?>&sort=created">Created</a></th>
+	</tr>
+<?php
+$handle = fopen("/var/reprepro/linkeddata/dists/ldstack" . $suiteSuffix . "/main/binary-amd64/Packages", "r");
+if ($handle)
+{
+	while (($line = fgets($handle, 1024)) !== false)
+	{
+		if (strlen($line) <= 2) // lines include EOL
+		{
+			if (count($row) == 3)
+			{
+				$created = filemtime("/var/reprepro/linkeddata/" . $row["Filename"]);
+				unset($row["Filename"]);
+				$row["Created"] = $created;
+
+				$rows[] = $row;
+			}
+
+			unset($row);
+		}
+		elseif (preg_match("/^(Package|Version|Filename):\s*(.+)$/", $line, $matches))
+			$row[$matches[1]] = $matches[2];
+	}
+
+	fclose($handle);
+
+	if ($_GET["sort"] == "created")
+	{
+		function cmp($a, $b)
+		{
+			return $b["Created"] - $a["Created"];
+		}
+
+		usort($rows, cmp);
+	}
+
+	foreach ($rows as $row)
+	{
+?>
+	<tr>
+		<td><?= $row["Package"] ?></td>
+		<td><?= $row["Version"] ?></td>
+		<td><?= date("Y-M-d H:i", $row["Created"]) ?></td>
+	</tr>
+<?php
+	}
+}
+?>
+</table>
+
+</body>
+</html>
---
+++ download/rss.php
@@ -0,0 +1,70 @@
+﻿<?php
+header("Content-Type: application/rss+xml; charset=UTF-8");
+
+$suite = $_GET["suite"];
+switch ($suite)
+{
+case "nightly":
+	$suiteSuffix = "-nightly";
+	break;
+case "testing":
+	$suiteSuffix = "-testing";
+	break;
+default:
+	$suite		 = "stable";
+	$suiteSuffix = "";
+}
+
+echo '<?xml version="1.0" encoding="utf-8"?>';
+?>
+
+<rss version="2.0">
+	<channel>
+		<title>Linked Data Stack: <?= ucfirst($suite) ?> Repository</title>
+		<description>The <?= $suite ?> debian repository of the Linked Data Stack.</description>
+		<link>http://stack.linkeddata.org/download/repo.php?suite=<?= $suite ?></link>
+<?php
+$handle = fopen("/var/reprepro/linkeddata/dists/ldstack" . $suiteSuffix . "/main/binary-amd64/Packages", "r");
+if ($handle)
+{
+	while (($line = fgets($handle, 1024)) !== false)
+	{
+		if (strlen($line) <= 2) // lines include EOL
+		{
+			if (count($row) == 3)
+			{
+				$created = filemtime("/var/reprepro/linkeddata/" . $row["Filename"]);
+				unset($row["Filename"]);
+				$row["Created"] = $created;
+
+				$rows[] = $row;
+			}
+
+			unset($row);
+		}
+		elseif (preg_match("/^(Package|Version|Filename):\s*(.+)$/", $line, $matches))
+			$row[$matches[1]] = $matches[2];
+	}
+
+	fclose($handle);
+
+	function cmp($a, $b)
+	{
+		return $b["Created"] - $a["Created"];
+	}
+
+	usort($rows, cmp);
+
+	foreach ($rows as $row)
+	{
+?>
+		<item>
+			<title><?= $row["Package"] ?> <?= $row["Version"] ?></title>
+			<pubDate><?= gmdate("D, d M Y H:i:s \G\M\T", $row["Created"]) ?></pubDate>
+		</item>
+<?php
+	}
+}
+?>
+	</channel>
+</rss>
